7c604ff34c19edc8ecd313b4baa7c281f134f49f
SujalTripathi <SujalTripathi@users.noreply.github.com>
2026-03-08 18:55:08 +0530

    add app env

--- server/app.js
+++ server/app.js
@@ -12,7 +12,15 @@ import templateRoutes from './routes/template.routes.js';
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://bill-generator-347e.onrender.com',
+    process.env.CLIENT_URL
+  ],
+  credentials: true
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
